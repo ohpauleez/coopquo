2c5517c0512efe805b70bc422b5777b170ea28cb
Paul deGrandis <paul.degrandis@gmail.com>
2011-01-20 14:25:12 -0800

    An easy function call to unmap all namespaces.  Useful in refactoring while working out of the repl

--- script/repl_init.clj
+++ script/repl_init.clj
@@ -2,3 +2,6 @@
   (:use [clojure.contrib.repl-utils :only (show)]))
 ;(clojure.contrib/repl-utils/add-break-thread!)
 
+(defn unmap-all-ns []
+  (doseq  [[f _]  (ns-publics *ns*)]  (ns-unmap *ns* f)))
+
