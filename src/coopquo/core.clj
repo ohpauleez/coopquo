ecb5ce73a5134c4e6024f291cbe18365d21706f6
Paul deGrandis <paul.degrandis@gmail.com>
2011-02-04 09:20:32 -0800

    Basic Pearson similarity and useful math functions for item and user based filtering (todo)

---
+++ src/coopquo/core.clj
@@ -0,0 +1,45 @@
+(ns coopquo.core
+  #_(:require [clojure.contrib.math :as math]))
+
+(defn similar-subset
+  "Find the similar subset of keys given two maps"
+  [id-1 id-2]
+  (clojure.set/intersection (set (keys id-1)) (set (keys id-2)))) ; faster than filter
+
+(defn similar-sum
+  "Sum all the preferences for user/ID found in the sub-preferences"
+  [prefs id sub-prefs]
+  #_(reduce #(+ (get-in prefs [id %2]) %1) 0 sub-prefs) ; slower, not as clear upfront
+  (apply + (vals (select-keys (prefs id) sub-prefs))))
+
+(defn similar-sum-square
+  "Sum the squares of all the preferences for user/ID found in the sub-preferences"
+  [prefs id sub-prefs]
+  #_(apply + (for [x (vals (select-keys (prefs id) sub-prefs))] (Math/pow x 2))) ; slower
+  #_(apply + (map #(Math/pow (get-in prefs [id %1]) 2) sub-prefs)) ; about the same as below for small sample sizes
+  #_(reduce #(+ (Math/pow (get-in prefs [id %2]) 2) %1) 0 sub-prefs) ; I think the above reads easier than this
+  (apply + (map #(Math/pow (get-in prefs [id %1]) 2) sub-prefs)))
+ 
+(defn similiar-sum-product
+  ""
+  [prefs id-1 id-2 sub-prefs]
+  #_(reduce #(+ (* (get-in prefs [id-1 %2]) (get-in [id-2 %2])) %1) 0 sub-prefs) ; this feels needlessly complex
+  (apply + (map #(* (get-in prefs [id-1 %2]) (get-in [id-2 %2])) sub-prefs)))
+
+(defn pearson
+  "Pearson Correltion between two users/IDs, given a preference map"
+  [prefs id-1 id-2]
+  (let [shared-items (similar-subset (prefs id-1) (prefs id-2))]
+    (if-let [n-shared (if (empty? shared-items) false (count shared-items))]
+      (let [sum1 (similar-sum prefs id-1 shared-items)
+            sum2 (similar-sum prefs id-2 shared-items)
+            sum1sq (similar-sum-square prefs id-1 shared-items)
+            sum2sq (similar-sum-square prefs id-2 shared-items)
+            psum (similiar-sum-product prefs id-1 id-2 shared-items)]
+        (try
+          (/ (- psum (/ (* sum1 sum2) n-shared))
+             (* (- sum1sq (/ (Math/pow sum1 2) n-shared))
+                (- sum2sq (/ (Math/pow sum2 2) n-shared))))
+          (catch Exception e 0)))
+      0)))
+
---
+++ test/coopquo/test/core.clj
@@ -0,0 +1,72 @@
+(ns coopquo.test.core
+  (:require [coopquo.core :as ccore] :reload)
+  (:use [lazytest.deftest :only (deftest is)]
+        [lazytest.describe :only  (describe it testing)]))
+
+;; This test set is based on the 'critics' set used in Programming Collective Intelligence (page 8)
+(def test-set {"Johhny"   {"The Life Aquatic" 5.0 "Amelie" 4.5 "Wall-E" 4.0
+                           "Mean Girls" 4.5 "Roman Holiday" 4.0 "White Christmas" 3.5
+                           "Pillow Talk" 4.0 "The Aristocats" 3.5}
+               "Mikey"    {"The Life Aquatic" 3.0 "Amelie" 5.0 "Wall-E" 5.0
+                           "Roman Holiday" 3.0 "White Christmas" 3.0 "The Aristocats" 2.0}
+               "Susan"    {"The Life Aquatic" 4.0 "Wall-E" 4.0
+                           "Mean Girls" 5.0 "Roman Holiday" 4.0
+                           "Pillow Talk" 4.0 "The Aristocats" 5.0}
+               "Marge"    {"The Life Aquatic" 4.0 "Amelie" 3.5 "Wall-E" 5.0
+                           "Mean Girls" 5.0 "Roman Holiday" 5.0 "White Christmas" 3.0
+                           "Pillow Talk" 4.0 "The Aristocats" 4.5}
+               "Toby"     {"Wall-E" 3.0 "Mean Girls" 5.0 "White Christmas" 3.0
+                           "Pillow Talk" 2.0}
+               "James"    {"The Life Aquatic" 1.0 "Amelie" 1.5  
+                           "Roman Holiday" 4.0}
+               "Jessica"  {"The Life Aquatic" 5.0 "Amelie" 5.0}
+               "Jane"     {"Amelie" 2.0 "Wall-E" 3.0 "Mean Girls" 4.5 "Roman Holiday" 4.0 
+                           "Pillow Talk" 1.0}})
+
+;; This smaller data set is used to ensure there are no issues with numeric keys/ids
+;;  The smaller set also makes it easier to compute operations/functions by hand
+(def test-set-nums {1 {"1" 5.0 "2" 3.0 "3" 1.0}
+                    2 {"2" 4.0 "4" 5.0 "5" 5.0}
+                    3 {"1" 5.0 "4" 4.5 "5" 4.0 "6" 4.0}
+                    4 {"1" 4.0}})
+
+;(deftest replace-me ;; FIXME: write
+;  (is false "No tests have been written."))
+
+(describe "Similar subset"
+  (testing "when there are no common keys"
+    (it "should return an empty set"
+      (let [m1 (test-set-nums 2)
+            m2 (test-set-nums 4)
+            expected #{}
+            experimental (ccore/similar-subset m1 m2)]
+        (= expected experimental))))
+  (testing "when the maps share keys"
+    (it "should return a set of those keys"
+      (let [m1 (test-set-nums 2)
+            m2 (test-set-nums 3)
+            expected #{"4" "5"}
+            experimental (ccore/similar-subset m1 m2)]
+        (= expected experimental))))
+  (testing "when the supplied args are similar (identical)"
+    (it "should return all of the keys"
+      (let [m1 (test-set-nums 2)
+            m2 {"2" 4.0 "4" 5.0 "5" 5.0}
+            expected #{"2" "4" "5"}
+            experimental (ccore/similar-subset m1 m2)]
+        (= expected experimental))))
+  (testing "when the supplied args are empty"
+    (it "should return an empty set"
+      (let [m1 {}
+            m2 {}
+            expected #{}
+            experimental (ccore/similar-subset m1 m2)]
+        (= expected experimental))))
+  (testing "when the one or both of the supplied args are nil"
+    (it "should throw an exception"
+      (let [m1 nil
+            m2 {}
+            expected #{}
+            experimental (ccore/similar-subset m1 m2)]
+        (= expected experimental)))))
+
