1b034ce4685e726a1358d844adae94095e39d4b3
Paul deGrandis <paul.degrandis@gmail.com>
2011-02-04 09:34:12 -0800

    A useful way to see the current bindings within a macro (like let)

--- script/repl_init.clj
+++ script/repl_init.clj
@@ -5,3 +5,7 @@
 (defn unmap-all-ns []
   (doseq  [[f _]  (ns-publics *ns*)]  (ns-unmap *ns* f)))
 
+;user=>  (let  [band "zeppelin" city "london"]  (show-env))
+;{city #<LocalBinding clojure.lang.Compiler$LocalBinding@78ff9053>, band #<LocalBinding clojure.lang.Compiler$LocalBinding@525c7734>}
+(defmacro show-env [] (println &env))
+
