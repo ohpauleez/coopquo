3efd8997a1b23080887677c8e0ee6502fba236e9
Paul deGrandis <paul.degrandis@gmail.com>
2011-02-04 13:13:38 -0800

    Some more one-off functions for repl use

--- script/repl_init.clj
+++ script/repl_init.clj
@@ -7,5 +7,12 @@
 
 ;user=>  (let  [band "zeppelin" city "london"]  (show-env))
 ;{city #<LocalBinding clojure.lang.Compiler$LocalBinding@78ff9053>, band #<LocalBinding clojure.lang.Compiler$LocalBinding@525c7734>}
-(defmacro show-env [] (println &env))
+;(defmacro show-env [] (println &env))
+(defmacro show-env []
+  (println  (keys &env)) `(println ~@(keys &env)))
+
+;(println  (+ (* 2 3) (dbg  (* 8 9))))
+;(println  (dbg (println "yo")))
+(defmacro dbg [x]
+  `(let  [x# ~x]  (println "dbg:" '~x "=" x#) x#))
 
